7a00d3e70780f8f467c2f53ae37912255512299f
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-05-14 22:38:41 -0700

    add table-driven testing, entries for literal-based matching

--- test/test-match-td.rb
+++ test/test-match-td.rb
@@ -25,4 +25,29 @@ ne3       ["anything"],      [nil, nil],      [nil, nil]
 ne3( {foo: "anything"},      {bar: nil},      {bar: nil} ) # name mismatch
 #=========================================================
   end
+
+  def test_literal
+#=========================================================
+#=            LITERAL |             ROT |            POT
+#=========================================================
+#= 2. every test that can be expressed in terms of a literal value in the
+#=    template
+#=
+literals = [
+  true, false,
+  42, -1.2e13, "baz",
+  [1,2,3], {a: 1, b: 2}
+]
+literals.each do |x1|
+  literals.each do |x2|
+    if x1 == x2
+      eq3         [x1],            [x1],   [{value: x1}]
+    else
+      ne3         [x1],            [x2],   [{value: x2}]
+    end
+    eq3       [x1, x2],        [x1, x2],   [{value: x1}, {value: x2}]
+  end
+end
+#=========================================================
+  end
 end
