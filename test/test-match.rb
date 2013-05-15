930c48bfb57518d07045ea4f410a46226a020236
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-05-14 23:36:35 -0700

    remove old test

--- test/test-match.rb
+++ test/test-match.rb
@@ -1,91 +1,162 @@
 require 'minitest/autorun'
 require 'object-template'
-require 'assert-threequal'
+require 'eq3'
+require 'set'
+
+class Member < Set ## ?
+  def === other
+    member? other
+  end
+end
 
 class TestMatch < Minitest::Test
-  include AssertThreequal
+  include Eq3
   
-  POT = PortableObjectTemplate
-  
-  attr_reader :spec, :template
-  
-  def setup
-    @spec =
-      [ {value: "foo"},
-        nil,
-        {set: ["red", "green"]},
-        {type: "number"},
-        {type: "number", range: [1,100]} ]
-    @template = POT.new spec
+  def test_cardinality
+#=========================================================
+#=            LITERAL |             ROT |            POT
+#=========================================================
+#= 1. every test that can be expressed in terms of presence of column/field
+#=    or cardinality of columns/fields, without matching values
+#=
+eq3                 [],              [],              []
+eq3       ["anything"],           [nil],           [nil]
+eq3   ["any", "thing"],      [nil, nil],      [nil, nil]
+#=========================================================
+ne3       ["anything"],              [],              []
+ne3                 [],           [nil],           [nil] # cardinality mismatch
+ne3   ["any", "thing"],           [nil],           [nil] # ditto
+ne3       ["anything"],      [nil, nil],      [nil, nil]
+#=========================================================
+#= 1a. specific to hashes
+ne3( {foo: "anything"},      {bar: nil},      {bar: nil} ) # name mismatch
+#=========================================================
   end
 
-  def test_match
-    [
-      [ "foo", [1,2,"three"], "green", 5.02, 42 ],
-      [ "foo", {}, "red", -99, 99 ]
-    ].each do |obj|
-      assert_threequal template, obj
+  def test_literal
+#=========================================================
+#=            LITERAL |             ROT |            POT
+#=========================================================
+#= 2. every test that can be expressed in terms of a literal value in the
+#=    template
+#=
+literals = [
+  true, false,
+  42, -1.2e13, "baz",
+  [1,2,3], {a: 1, b: 2}
+]
+literals.each do |x1|
+  literals.each do |x2|
+    if x1 == x2
+      eq3         [x1],            [x1],   [{value: x1}]
+    else
+      ne3         [x1],            [x2],   [{value: x2}]
     end
+    eq3       [x1, x2],        [x1, x2],   [{value: x1}, {value: x2}]
   end
-  
-  def test_not_match
-    [
-      [ "bar", {}, "red",  -99, 1 ],
-      [ "foo", {}, "blue", -99, 1 ],
-      [ "foo", {}, "red", "99", 1 ],
-      [ "foo", {}, "red",  -99, 0 ],
-      [ "foo" ],
-      [ "foo", {}, "red",  -99, 1, "extra" ],
-      [ ],
-    ].each do |obj|
-      assert_not_threequal template, obj
-    end
+end
+#=========================================================
   end
-  
-  def test_match_types
-    assert_threequal(
-      POT.new([{type: "string"}]),
-      [ "some string" ]
-    )
-    assert_threequal(
-      POT.new([{type: "number"}]),
-      [ 42 ]
-    )
-    assert_threequal(
-      POT.new([{type: "list"}]),
-      [ [1,2,3] ]
-    )
-    assert_threequal(
-      POT.new([{type: "map"}]),
-      [ {a: 1} ]
-    )
-    
-    assert_not_threequal(
-      POT.new([{type: "string"}]),
-      [ 12 ]
-    )
-    assert_not_threequal(
-      POT.new([{type: "number"}]),
-      [ "42" ]
-    )
-    assert_not_threequal(
-      POT.new([{type: "list"}]),
-      [ {a: 1} ]
-    )
-    assert_not_threequal(
-      POT.new([{type: "map"}]),
-      [ [1,2,3] ]
-    )
+
+  def test_set
+#=========================================================
+#=            LITERAL |             ROT |            POT
+#=========================================================
+#= 3. every test that can be expressed in terms of a set of values in the
+#=    template
+#=
+eq3                [1], [Member[0,1,2]], [{set: [0,1,2]}]
+#=========================================================
+ne3                [4], [Member[0,1,2]], [{set: [0,1,2]}]
+#=========================================================
   end
-  
+
+  def test_range
+#=========================================================
+#=            LITERAL |             ROT |            POT
+#=========================================================
+#= 4. every test that can be expressed in terms of a range in the
+#=    template
+#=
+eq3                [0],          [0..2], [{range: [0,2]}]
+eq3                [2],          [0..2], [{range: [0,2]}]
+#=========================================================
+ne3               [-1],          [0..2], [{range: [0,2]}]
+ne3                [3],          [0..2], [{range: [0,2]}]
+ne3                [2],         [0...2], [{range: [0,2, true]}]
+#=========================================================
+  end
+
   def test_regex
-    assert_threequal(
-      POT.new([{type: "string", regex: "^f...r$"}]),
-      [ "fubar" ]
-    )
-    assert_not_threequal(
-      POT.new([{type: "string", regex: "^f...r$"}]),
-      [ "zfubarz" ]
-    )
+#=========================================================
+#=            LITERAL |             ROT |            POT
+#=========================================================
+#= 5. every test that can be expressed in terms of a regex in the
+#=    template
+#=
+eq3            ["foo"],          [/oo/], [{regex: "oo"}]
+#=========================================================
+ne3            ["foo"],          [/zz/], [{regex: "zz"}]
+#=========================================================
+  end
+
+  def test_type
+#=========================================================
+#=            LITERAL |             ROT |            POT
+#=========================================================
+#= 6. every test that can be expressed in terms of a type in the
+#=    template
+#=
+eq3               [42],       [Numeric], [{type: "number"}]
+eq3            ["foo"],        [String], [{type: "string"}]
+eq3          [[1,2,3]],         [Array], [{type: "list"}]
+eq3       [{a:1, b:2}],         [Hash],  [{type: "map"}]
+#=========================================================
+ne3             ["42"],       [Numeric], [{type: "number"}]
+ne3              [123],        [String], [{type: "string"}]
+ne3       [{a:1, b:2}],         [Array], [{type: "list"}]
+ne3          [[1,2,3]],         [Hash],  [{type: "map"}]
+#=========================================================
+  end
+
+  def test_conjunction
+    ## note that ROT doesn't support conjunctions, though it would be easy
+    ## to implement.
+    eq3 ["abc"], [nil], [{type: "string", regex: "b", set: ["abc", "def"]}]
+    ne3 ["abc"],   [0], [{type: "list",   regex: "b", set: ["abc", "def"]}]
+    ne3 ["abc"],   [0], [{type: "string", regex: "d", set: ["abc", "def"]}]
+    ne3 ["abc"],   [0], [{type: "string", regex: "b", set: ["xyz", "def"]}]
+  end
+
+  # test that a mismatch in one entry makes the template match fail
+  def test_all_entries
+    eq3 [                5,           1.23,            "bar",         "baz",          [1,2],          {a: 1} ],
+        [          Numeric,           1..3,           String,          /az/,          Array,          {a: 1} ],
+        [ {type: "number"}, {range: [1,3]}, {type: "string"}, {regex: "az"}, {type: "list"}, {value: {a: 1}} ]
+
+    ne3 [            "foo",           1.23,            "bar",         "baz",          [1,2],          {a: 1} ],
+        [          Numeric,           1..3,           String,          /az/,          Array,          {a: 1} ],
+        [ {type: "number"}, {range: [1,3]}, {type: "string"}, {regex: "az"}, {type: "list"}, {value: {a: 1}} ]
+
+    ne3 [                5,           4.23,            "bar",         "baz",          [1,2],          {a: 1} ],
+        [          Numeric,           1..3,           String,          /az/,          Array,          {a: 1} ],
+        [ {type: "number"}, {range: [1,3]}, {type: "string"}, {regex: "az"}, {type: "list"}, {value: {a: 1}} ]
+
+    ne3 [                5,           1.23,          ["bar"],         "baz",          [1,2],          {a: 1} ],
+        [          Numeric,           1..3,           String,          /az/,          Array,          {a: 1} ],
+        [ {type: "number"}, {range: [1,3]}, {type: "string"}, {regex: "az"}, {type: "list"}, {value: {a: 1}} ]
+
+    ne3 [                5,           1.23,            "bar",         "bAz",          [1,2],          {a: 1} ],
+        [          Numeric,           1..3,           String,          /az/,          Array,          {a: 1} ],
+        [ {type: "number"}, {range: [1,3]}, {type: "string"}, {regex: "az"}, {type: "list"}, {value: {a: 1}} ]
+
+    ne3 [                5,           1.23,            "bar",         "baz",          {a:2},          {a: 1} ],
+        [          Numeric,           1..3,           String,          /az/,          Array,          {a: 1} ],
+        [ {type: "number"}, {range: [1,3]}, {type: "string"}, {regex: "az"}, {type: "list"}, {value: {a: 1}} ]
+
+    ne3 [                5,           1.23,            "bar",         "baz",          [1,2],          {a: 2} ],
+        [          Numeric,           1..3,           String,          /az/,          Array,          {a: 1} ],
+        [ {type: "number"}, {range: [1,3]}, {type: "string"}, {regex: "az"}, {type: "list"}, {value: {a: 1}} ]
+
   end
 end
